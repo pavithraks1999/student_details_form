5a314e76b4af519bc381564c400c647248ebe536
PAVITHRA K S <kspavi1999@gmail.com>
2024-12-31 23:01:01 +0530

    Update and rename html to model/Student.cs

---
+++ model/Student.cs
@@ -0,0 +1,11 @@
+namespace WebApplication13.Models // Ensure the namespace matches your project
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Course { get; set; }
+    }
+}
+
